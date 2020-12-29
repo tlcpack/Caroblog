1c37e139735caed481e06d1feaff3b791fc8c64e
Taylor Cooke <tcooke@relias.com>
2020-12-29 13:24:59 -0500

    minor refactoring

--- caroblog/src/components/Index.vue
+++ caroblog/src/components/Index.vue
@@ -2,31 +2,33 @@
   <div class="pt-5">
     <div v-if="posts && posts.length">
       <b-jumbotron class="mb-3" v-for="post of posts" v-bind:key="post.id">
-              <h5 class="display-4">{{ post.title }}</h5>
-              <p class="lead">{{ post.content }}</p>
-              <b-button
-                :to="{ name: 'edit', params: { id: post.id } }"
-                class="lead btn btn-lg"
-                variant="primary"
-                >Edit</b-button
-              >
-              <b-button
-                :to="{ name: 'details', params: { id: post.id } }"
-                class="lead btn btn-lg ml-1"
-                variant="info"
-                >Details</b-button
-              >
-              <b-button
-                class="lead btn-lg ml-1"
-                v-on:click="deletePost(post)"
-                variant="danger"
-              >
-                Delete
-              </b-button>
+        <h5 class="display-4">{{ post.title }}</h5>
+        <p class="lead">{{ post.content }}</p>
+        <b-button
+          :to="{ name: 'edit', params: { id: post.id } }"
+          class="lead btn btn-lg"
+          variant="primary"
+          >Edit</b-button
+        >
+        <b-button
+          :to="{ name: 'details', params: { id: post.id } }"
+          class="lead btn btn-lg ml-1"
+          variant="info"
+          >Details</b-button
+        >
+        <b-button
+          class="lead btn-lg ml-1"
+          v-on:click="deletePost(post)"
+          variant="danger"
+        >
+          Delete
+        </b-button>
       </b-jumbotron>
     </div>
     <div class="d-flex justify-content-center mb-3">
-    <b-spinner style="width: 3rem; height: 3rem;" v-if="posts.length == 0">No Posts</b-spinner>
+      <b-spinner style="width: 3rem; height: 3rem" v-if="posts.length == 0"
+        >No Posts</b-spinner
+      >
     </div>
   </div>
 </template>
@@ -34,11 +36,7 @@
 <script>
 import axios from "axios";
 
-
 export default {
-  props: {
-    loading: {type: Boolean}
-  },
   data() {
     return {
       posts: [],
